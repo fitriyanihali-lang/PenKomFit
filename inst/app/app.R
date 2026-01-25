bfea04eb7b62ce2eb08bafd2267140d37077be72
fitriyanihali-lang <fitriyanihali@gmail.com>
2026-01-25 18:11:51 +0700

    Update app.R

--- inst/app/app.R
+++ inst/app/app.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(openxlsx)
 
 ui <- fluidPage(
-  theme = shinytheme("superhero"),
+  theme = shinytheme("cosmo"),
   titlePanel("Pengolahan Data Kemampuan dan Nilai Tes Pengukuran Penalaran Kombinatorial"),
   sidebarLayout(
     sidebarPanel(
