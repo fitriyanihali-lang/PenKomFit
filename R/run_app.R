fc037f67dcb375412c0f8b0a164ddbf344416225
fitriyanihali-lang <fitriyanihali@gmail.com>
2026-02-25 09:10:45 +0700

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,11 +1,11 @@
 #' Jalankan Aplikasi Pengolahan Skor Kombinatorial
 #'
-#' Fungsi ini akan membuka aplikasi Shiny untuk mengolah data siswa berdasarkan model GPCM.
+#' Fungsi ini membuka aplikasi Shiny untuk mengolah data siswa berdasarkan model IRT GPCM.
 #' @export
 run_kombinatorial_app <- function() {
   appDir <- system.file("app", package = "PenKomFit")
-  if (appDir == "") {
-    stop("App tidak ditemukan dalam package.")
+  if (appDir == "" || !dir.exists(appDir)) {
+    stop("Folder app tidak ditemukan. Pastikan file ada di inst/app/ dalam package.")
   }
   shiny::runApp(appDir, display.mode = "normal")
 }
